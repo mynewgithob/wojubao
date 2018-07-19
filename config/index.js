fc1b371642825a42d533d45a083d4d8bef580d2e
mynewgithob <1039468294@qq.com>
2018-07-19 17:10:28 +0800

    wn

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:80'
+        target: 'http://localhost:8080'
       }
     },
 
--- src/main.js
+++ src/main.js
@@ -5,11 +5,12 @@ import App from './App'
 import router from './router'
 import fastClick from 'fastclick' //解决手机端300ms延迟问题
 import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'babel-polyfill'
 import store from './store'
 import 'styles/border.css'  //兼容浏览器1px问题
 import 'styles/reset.css' //重置文件
 import 'styles/iconfont.css' //使用iconfont图标
-import '../node_modules/vue-awesome-swiper/node_modules/swiper/dist/css/swiper.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
